48a2b10f3f8ec65a6d99a22597657a3f2b0f14e7
Guillermo Cerame <g.cerame@gmail.com>
2022-06-05 15:47:31 +0200

    created base html layout for all pages

--- views/admin/auth/signin.js
+++ views/admin/auth/signin.js
@@ -1,11 +1,16 @@
+const layout = require('../layout');
+
+//language=HTML
 module.exports = () => {
-    return `
-    <div>
-            <form method="POST">
-                <input name="email" placeholder="email"/>
-                <input name="password" placeholder="password"/>
-                <button>Sign in</button>
-            </form>
-        </div>
-    `
+    return layout({
+        content: `
+            <div>
+                <form method="POST">
+                    <input name="email" placeholder="email"/>
+                    <input name="password" placeholder="password"/>
+                    <button>Sign in</button>
+                </form>
+            </div>
+        `
+    });
 };
---
+++ views/admin/layout.js
@@ -0,0 +1,16 @@
+//language=HTML
+
+module.exports = ({content}) => {
+    return `
+        <!DOCTYPE html>
+        <html lang="en">
+            <head>
+               <title>E-commerce</title> 
+            </head>
+            <body>
+            
+            </body>
+        </html>
+        
+    `
+};
