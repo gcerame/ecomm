d989e743128ef328595abf467c4fa7ec8fcfb454
Guillermo Cerame <g.cerame@gmail.com>
2022-06-08 19:54:09 +0200

    added product creation

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cookieSession = require('cookie-session');
-const authRouter = require('./routes/admin/auth')
+const authRouter = require('./routes/admin/auth');
+const productsRouter = require('./routes/admin/products')
 
 const app = express();
 app.use(express.static('public'));
-app.use(
-    bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(
     cookieSession({
         keys: ['d82ydhad782yd7d23fgcxa']
     }));
 app.use(authRouter);
-
+app.use(productsRouter);
 app.listen(3000, () => {
     console.log('Listening');
 });
