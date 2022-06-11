fe2ee77cc050bbf910bd2be97e7f65b47a88eda8
ceramesupersound <g.cerame@gmail.com>
2022-06-11 22:43:48 +0200

    adding to cart now redirects to /cart

--- routes/carts.js
+++ routes/carts.js
@@ -5,50 +5,53 @@ const cartShowTemplate = require('../views/carts/show');
 
 const router = express.Router();
 //Add item to cart
-router.post('/cart/products', async (req, res) => {
-    //Create cart if it doesnt exist, or get the existing cart
-    let cart;
-    if (!req.session.cartId) {
-        cart = await cartsRepo.create({items: []});
-        req.session.cartId = cart.id;
-    } else {
-        cart = await cartsRepo.getOne(req.session.cartId);
-    }
-    const existingItem = cart.items.find(item => item.id === req.body.productId);
-    if (existingItem) {
-        existingItem.quantity++;
-    } else {
-        cart.items.push({id: req.body.productId, quantity: 1});
-    }
-    await cartsRepo.update(cart.id, {
-        items: cart.items
+router.post('/cart/products',
+    async (req, res) => {
+        //Create cart if it doesnt exist, or get the existing cart
+        let cart;
+        if (!req.session.cartId) {
+            cart = await cartsRepo.create({items: []});
+            req.session.cartId = cart.id;
+        } else {
+            cart = await cartsRepo.getOne(req.session.cartId);
+        }
+        const existingItem = cart.items.find(item => item.id === req.body.productId);
+        if (existingItem) {
+            existingItem.quantity++;
+        } else {
+            cart.items.push({id: req.body.productId, quantity: 1});
+        }
+        await cartsRepo.update(cart.id, {
+            items: cart.items
+        });
+        res.redirect('/cart');
     });
-    res.send('Added to cart');
-});
 //Show all items in cart
-router.get('/cart', async (req, res) => {
-    if (!req.session.cartId) {
-        return res.redirect('/');
-    }
-    const cart = await cartsRepo.getOne(req.session.cartId);
+router.get('/cart',
+    async (req, res) => {
+        if (!req.session.cartId) {
+            return res.redirect('/');
+        }
+        const cart = await cartsRepo.getOne(req.session.cartId);
 
-    for (let item of cart.items) {
-        const product = await productsRepo.getOne(item.id);
-        item.product = product;
-    }
-    res.send(cartShowTemplate({items: cart.items}));
-});
+        for (let item of cart.items) {
+            const product = await productsRepo.getOne(item.id);
+            item.product = product;
+        }
+        res.send(cartShowTemplate({items: cart.items}));
+    });
 
 //Delete an item from cart
-router.post('/cart/products/delete', async (req,res) => {
-    const {itemId} = req.body;
-    const cart = await cartsRepo.getOne(req.session.cartId);
+router.post('/cart/products/delete',
+    async (req, res) => {
+        const {itemId} = req.body;
+        const cart = await cartsRepo.getOne(req.session.cartId);
 
-    const items = cart.items.filter(item=> item.id !== itemId);
+        const items = cart.items.filter(item => item.id !== itemId);
 
-    await cartsRepo.update(req.session.cartId, {items});
+        await cartsRepo.update(req.session.cartId, {items});
 
-    res.redirect('/cart');
+        res.redirect('/cart');
 
-})
+    })
 module.exports = router;
