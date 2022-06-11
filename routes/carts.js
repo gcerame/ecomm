fa5dea801eb81e3d57824ced3c9505c6e95cd5fa
ceramesupersound <g.cerame@gmail.com>
2022-06-11 22:33:27 +0200

    added product deletion from cart

--- routes/carts.js
+++ routes/carts.js
@@ -40,5 +40,15 @@ router.get('/cart', async (req, res) => {
 });
 
 //Delete an item from cart
-router.post('')
+router.post('/cart/products/delete', async (req,res) => {
+    const {itemId} = req.body;
+    const cart = await cartsRepo.getOne(req.session.cartId);
+
+    const items = cart.items.filter(item=> item.id !== itemId);
+
+    await cartsRepo.update(req.session.cartId, {items});
+
+    res.redirect('/cart');
+
+})
 module.exports = router;
