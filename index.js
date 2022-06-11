401b257376a8e9cfd7467fbdbd1b796e166d1b0f
Guillermo Cerame <g.cerame@gmail.com>
2022-06-11 12:32:47 +0200

    working on shopping cart

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cookieSession = require('cookie-session');
+
 const authRouter = require('./routes/admin/auth');
 const adminProductsRouter = require('./routes/admin/products')
 const productsRouter = require('./routes/products');
+const cartsRouter = require('./routes/carts');
+
 const app = express();
+
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(
@@ -14,6 +18,9 @@ app.use(
 app.use(authRouter);
 app.use(adminProductsRouter);
 app.use(productsRouter);
+app.use(cartsRouter);
+
+
 app.listen(3000, () => {
     console.log('Listening');
 });
---
+++ routes/carts.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const cartsRepo = require('../repositories/carts');
+const productsRepo = require('../repositories/products');
+const cartShowTemplate = require('../views/carts/show');
+
+const router = express.Router();
+//Add item to cart
+router.post('/cart/products', async (req, res) => {
+    //Create cart if it doesnt exist, or get the existing cart
+    let cart;
+    if (!req.session.cartId) {
+        cart = await cartsRepo.create({items: []});
+        req.session.cartId = cart.id;
+    } else {
+        cart = await cartsRepo.getOne(req.session.cartId);
+    }
+    const existingItem = cart.items.find(item => item.id === req.body.productId);
+    if (existingItem) {
+        existingItem.quantity++;
+    } else {
+        cart.items.push({id: req.body.productId, quantity: 1});
+    }
+    await cartsRepo.update(cart.id, {
+        items: cart.items
+    });
+    res.send('Added to cart');
+});
+//Show all items in cart
+router.get('/cart', async (req, res) => {
+    if (!req.session.cartId) {
+        return res.redirect('/');
+    }
+    const cart = await cartsRepo.getOne(req.session.cartId);
+
+    for (let item of cart.items) {
+        const product = await productsRepo.getOne(item.id);
+        item.product = product;
+    }
+    res.send(cartShowTemplate({items: cart.items}));
+});
+
+//Delete an item from cart
+router.post('')
+module.exports = router;
